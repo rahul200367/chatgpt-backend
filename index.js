f23bd7ad29efa0b3481931bea19c9c7d99097f8d
rahul <raulkaus07@gmail.com>
2023-06-10 23:38:46 +0530

    2it

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const configuration = new Configuration({
     organization: "org-dtV4tB2hmkBdIqsvLrjqqZBe",
-    apiKey:"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",
+    apiKey:"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",
 });
 const openai = new OpenAIApi(configuration);
 const app = express();
@@ -31,47 +31,4 @@ app.post('/',async (req,res)=>{
 app.listen(port, ()=>{
     console.log('app listening on port 5000')
 });
-
-/*const express = require('express')
-const axios = require('axios');
-const cors = require('cors')
-const app = express();
-const port = 5000;
-
-// Middleware to parse JSON request bodies
-app.use(express.json());
-app.use(cors())
-// API endpoint for handling incoming chat messages
-app.post('/', async (req, res) => {
-  try {
-    const { message } = req.body;
-
-    // Make a request to the ChatGPT API
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-      "prompt":message,
-      "max_tokens": 100,
-      "top_p": 1
-    }, {
-      headers: {
-        'Authorization':"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",
-        'Content-Type': 'application/json'
-      }
-    });
-
-    // Extract the generated chat message from the API response
-    const data = response.data.choices[0].text.trim();
-
-    // Send the generated reply back to the client
-    res.json({ data });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-    console.log({error})
-  }
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});*/
 
